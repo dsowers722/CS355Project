212dd2cb7001a87e073167bac2922c53ef1f0b3c
YufengQian <qian176@purdue.edu>
2023-11-27 15:33:38 -0500

    added one hash based encryption

--- test.py
+++ test.py
@@ -1,22 +1,5 @@
-'Chat Room Connection - Client-To-Client'
-import base64
-import threading
-import socket
 import hashlib
-host = '127.0.0.1'
-port = 59000
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
-clients = []
-aliases = []
-key_dictionary=[]
-
-def broadcast(message):
-    for client in clients:
-        client.send(message)
-
-# Function to handle clients'connections
+import base64
 
 def encrypt_message(message):
     # Encode the message to bytes
@@ -62,40 +45,17 @@ def decrypt_message(encrypted_message, key):
     decrypted_message = hashed_message_bytes.decode('utf-8')
 
     return decrypted_message
-def handle_client(client):
-    while True:
-        try:
-            message = client.recv(1024)
-            enc_message, key = encrypt_message(message)
-            key_dictionary.append(message, key)
-
-            broadcast(enc_message)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            alias = aliases[index]
-            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
-            aliases.remove(alias)
-            break
-# Main function to receive the clients connection
-
-
-def receive():
-    while True:
-        print('Server is running and listening ...')
-        client, address = server.accept()
-        print(f'connection is established with {str(address)}')
-        client.send('alias?'.encode('utf-8'))
-        alias = client.recv(1024)
-        aliases.append(alias)
-        clients.append(client)
-        print(f'The alias of this client is {alias}'.encode('utf-8'))
-        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
-        client.send('you are now connected!'.encode('utf-8'))
-        thread = threading.Thread(target=handle_client, args=(client,))
-        thread.start()
-
 
 if __name__ == "__main__":
-    receive()
+    message = 'I am a pig'
+
+    # Encrypt the message
+    encrypted_message, key = encrypt_message(message)
+
+    # Decrypt the message using the key
+    decrypted_message = decrypt_message(encrypted_message, key)
+
+    print("Original Message:", message)
+    print("Encrypted Message:", encrypted_message)
+    print("Decrypted Message:", decrypted_message)
+
